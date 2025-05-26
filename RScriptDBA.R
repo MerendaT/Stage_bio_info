e115e66195fbad1a30031e25dfa6315d1de4fbde
mathis.leclaire <mathis.leclaire@i2bc.paris-saclay.fr>
2025-05-26 10:17:22 +0200

    code cleanning

--- RScriptDBA.R
+++ RScriptDBA.R
@@ -1,34 +1,35 @@
 library(DiffBind)
 setwd("~/Stage")
 
-# create dataframe with 5 columns and 22 rows
+# create dataframe with 7 columns and 22 rows
 ## create variables
-sample_id <- c("TIR1_20_1", "TIR1_20_2", "TIR1_20_3", "TIR1_20_4", "TIR1_22_1",
-               "TIR1_22_2", "Smarca4_20_1", "Smarca4_20_2", "Smarca4_20_3",
-               "Smarca4_20_4", "ARID1A_22_1", "ARID1A_22_2", "BRD7_22_1",
-               "BRD7_22_2", "BRD9_22_1", "BRD9_22_2", "DPF2_22_1", "DPF2_22_2",
-               "PDRM1_22_1", "PDRM1_22_2", "SMARCB1_22_1", "SMARCB1_22_2")
-annee <- c("2020", "2020", "2020", "2020", "2022", "2022", "2020", "2020",
+SampleID <- c("TIR1_20_1", "TIR1_20_2", "TIR1_20_3", "TIR1_20_4", "TIR1_22_1",
+              "TIR1_22_2", "Smarca4_20_1", "Smarca4_20_2", "Smarca4_20_3",
+              "Smarca4_20_4", "ARID1A_22_1", "ARID1A_22_2", "BRD7_22_1",
+              "BRD7_22_2", "BRD9_22_1", "BRD9_22_2", "DPF2_22_1", "DPF2_22_2",
+              "PDRM1_22_1", "PDRM1_22_2", "SMARCB1_22_1", "SMARCB1_22_2")
+
+Annee <- c("2020", "2020", "2020", "2020", "2022", "2022", "2020", "2020",
            "2020", "2020", "2022", "2022", "2022", "2022", "2022", "2022",
            "2022", "2022", "2022", "2022", "2022", "2022")
-condition <- c("control", "control", "control", "control", "control",
+
+Condition <- c("control", "control", "control", "control", "control",
                "control", "Smarca4", "Smarca4", "Smarca4", "Smarca4",
                "ARID1A", "ARID1A", "BRD7", "BRD7", "BRD9", "BRD9",
                "DPF2", "DPF2", "PBRM1", "PBRM1", "SMARCB1", "SMARCB1")
-replicate <- c("1", "2", "3", "4", "1", "2", "1", "2", "3", "4", "1", "2",
+
+Replicate <- c("1", "2", "3", "4", "1", "2", "1", "2", "3", "4", "1", "2",
                "1", "2", "1", "2", "1", "2", "1", "2", "1", "2")
 
-## create dataframe
-table_dba_baf_complexes <- data.frame(sample_id, annee,
-                                      condition, replicate)
-
-print(table_dba_baf_complexes)
-
-save.image("EnvironmentDiffBindDBA.RData")
-
+PeakCaller <- c("narrowPeak", "narrowPeak", "narrowPeak", "narrowPeak",
+                "narrowPeak", "narrowPeak", "narrowPeak", "narrowPeak",
+                "narrowPeak", "narrowPeak", "narrowPeak", "narrowPeak",
+                "narrowPeak", "narrowPeak", "narrowPeak", "narrowPeak",
+                "narrowPeak", "narrowPeak", "narrowPeak", "narrowPeak",
+                "narrowPeak", "narrowPeak")
 
 ## Import narrowPeak files
-peak <- c(
+Peaks <- c(
   file.path("~/Stage/data_stage/chr18_peak_input2-4/chr18_TIR1_rep1_g2-3-4_INPUT_2020.narrowPeak"),
   file.path("~/Stage/data_stage/chr18_peak_input2-4/chr18_TIR1_rep2_g2-3-4_INPUT_2020.narrowPeak"),
   file.path("~/Stage/data_stage/chr18_peak_input2-4/chr18_TIR1_rep3_g2-3-4_INPUT_2020.narrowPeak"),
@@ -53,55 +54,8 @@ peak <- c(
   file.path("~/Stage/data_stage/chr18_peak_input2-4/chr18_SMARCB1-2-3-4-INPUT_rep2_C002GFA_.narrowPeak")
 )
 
-## Add peak in the table
-table_dba_baf_complexes <- data.frame(sample_id, annee,
-                                      condition, replicate, peak)
-
-print(table_dba_baf_complexes)
-
-
-# ## Import bam files
-# tir1_20_1_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_TIR1_rep1_g2-3-4_INPUT_2020.bam")
-# tir1_20_2_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_TIR1_rep2_g2-3-4_INPUT_2020.bam")
-# tir1_20_3_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_TIR1_rep3_g2-3-4_INPUT_2020.bam")
-# tir1_20_4_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_TIR1_rep4_g2-3-4_INPUT_2020.bam")
-# tir1_22_1_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_TIR1-2-3-4-INPUT_rep1_C002GF8_.bam")
-# tir1_22_2_bzm <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_TIR1-2-3-4-INPUT_rep2_C002GF7_.bam")
-# smarca4_20_1_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_SMARCA4_rep1_g2-3-4_INPUT_2020.bam")
-# smarca4_20_2_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_SMARCA4_rep2_g2-3-4_INPUT_2020.bam")
-# smarca4_20_3_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_SMARCA4_rep3_g2-3-4_INPUT_2020.bam")
-# smarca4_20_4_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_SMARCA4_rep4_g2-3-4_INPUT_2020.bam")
-# arid1a_22_1_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_ARID1A-2-3-4-INPUT_rep1_C002GEX_.bam")
-# arid1a_22_2_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_ARID1A-2-3-4-INPUT_rep2_C002GEY_.bam")
-# brd7_22_1_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_BRD7-2-3-4-INPUT_rep1_C002GF3_.bam")
-# brd7_22_2_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_BRD7-2-3-4-INPUT_rep2_C002GF4_.bam")
-# brd9_22_1_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_BRD9-2-3-4-INPUT_rep1_C002GF5_.bam")
-# brd9_22_2_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_BRD9-2-3-4-INPUT_rep2_C002GF6_.bam")
-# dpf2_22_1_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_DPF2-2-3-4-INPUT_rep1_C002GEZ_.bam")
-# dpf2_22_2_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_DPF2-2-3-4-INPUT_rep2_C002GF0_.bam")
-# pbrm1_22_1_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_PBRM1-2-3-4-INPUT_rep1_C002GF1_.bam")
-# pbrm1_22_2_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_PBRM1-2-3-4-INPUT_rep2_C002GF2_.bam")
-# smarcb1_22_1_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_SMARCB1-2-3-4-INPUT_rep1_C002GF9_.bam")
-# smarcb1_22_2_bam <- BamFile("/home/mathis.leclaire/Stage/data_stage/reads/chr18_SMARCB1-2-3-4-INPUT_rep2_C002GFA_.bam")
-
-# ## Put bam files in a variable
-# bamread <- c(tir1_20_1_bam, tir1_20_2_bam, tir1_2O_3_bam, tir1_20_4_bam,
-#               tir1_22_1_bam, tir1_22_2_bam, smarca4_20_1_bam, smarca4_20_2_bam,
-#               smarca4_20_3_bam, smarca4_20_4_bam, arid1a_22_1_bam,
-#               arid1a_22_2_bam, brd7_22_1_bam, brd7_22_2_bam, brd9_22_1_bam,
-#               brd9_22_2_bam, dpf2_22_1_bam, dpf2_22_2_bam, pbrm1_22_1_bam,
-#               pbrm1_22_2_bam, smarcb1_22_1_bam, smarcb1_22_2_bam)
-
-# ## Add bamRead in the table
-# table_dba_baf_complexes <- data.frame(sample_id, annee,
-#                                       condition, replicate, bam_read, peak)
-
-# print(table_dba_baf_complexes)
-
-
-
 ## Import bam files
-bam_read <- c(
+bamReads <- c(
     file.path("~/Stage/data_stage/reads/chr18_TIR1_rep1_g2-3-4_INPUT_2020.bam"),file.path("~/Stage/data_stage/reads/chr18_TIR1_rep2_g2-3-4_INPUT_2020.bam"),file.path("~/Stage/data_stage/reads/chr18_TIR1_rep3_g2-3-4_INPUT_2020.bam"),
     file.path("~/Stage/data_stage/reads/chr18_TIR1_rep4_g2-3-4_INPUT_2020.bam"),
     file.path("~/Stage/data_stage/reads/chr18_TIR1-2-3-4-INPUT_rep1_C002GF8_.bam"),
@@ -124,19 +78,19 @@ bam_read <- c(
     file.path("~/Stage/data_stage/reads/chr18_SMARCB1-2-3-4-INPUT_rep2_C002GFA_.bam")
 )
 
-## Add bamRead in the table
-table_dba_baf_complexes <- data.frame(sample_id, annee,
-                                      condition, replicate, bam_read, peak)
-
-print(table_dba_baf_complexes)
-
 ## Import BamControl files
-bam_read_ctl <- c(
+bamControl <- c(
     file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam"),file.path("~/Stage/data_stage/reads/chr18_merge6_TIR1-2-3-4-INPUT.bam")
 )
 
 ## Add bam_read_ctl in the table
-table_dba_baf_complexes <- data.frame(sample_id, annee, condition, replicate,
-                                      bam_read, bam_read_ctl, peak)
+table_dba_baf_complexes <- data.frame(SampleID, Annee, Condition, Replicate,
+                                      bamReads, bamControl, Peaks, PeakCaller)
 
-print(table_dba_baf_complexes)
+table_dba_baf_complexes
+
+write.csv(table_dba_baf_complexes, file = "~/Stage/dba_baf_complexes_table.csv")
+
+object_dba_baf_complexes <- dba(sampleSheet = "~/Stage/dba_baf_complexes_table.csv", scoreCol = 7)
+
+object_dba_baf_complexes
